fdead1b2f98f4ec558dbc9274f4c28b08ef9fe5a
Ernest Zet <ernest52@onet.pl>
2024-03-21 16:21:55 +0100

    Change htpp method for remove account and change some styles

--- views/routers/myAccount.js
+++ views/routers/myAccount.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const Router = express.Router();
-const { User, Movie, Review } = require("../../dataBase");
-const {
-  asyncWrap,
-  ErrorApp,
-  searcherValidator,
-} = require("../../public/scripts/ErrorApp");
+const { searcherValidator } = require("../../public/scripts/ErrorApp");
 const {
   myAccountRenderGet,
   serialSearcherPanel,
@@ -22,20 +17,28 @@ const {
   deleteFromFavourite,
 } = require("../../controllers/myAccount");
 
-Router.get("/", myAccountRenderGet);
-Router.post("/", searcherValidator, serialSearcherPanel);
+Router.route("/")
+  .get(myAccountRenderGet)
+  .post(searcherValidator, serialSearcherPanel);
+Router.route("/update").get(updateAccountForm).patch(updateAccount);
+Router.route("/comments/:id/update")
+  .get(commnetsUpdateForm)
+  .post(commentsUpdate);
+
+// Router.get("/", myAccountRenderGet);
+// Router.post("/", searcherValidator, serialSearcherPanel);
 Router.get("/settings", myAccountSettings);
-Router.post("/logout", logOut);
-Router.get("/remove", removeAccount);
-Router.get("/update", updateAccountForm);
-Router.patch("/update", updateAccount);
-Router.get("/comments", commentsShower);
-Router.get("/comments/:id/update", commnetsUpdateForm);
-Router.post("/comments/:id/update", commentsUpdate);
-Router.delete("/comments/:id/delete", commentsDelete);
 
 Router.get("/favourites/:id", addToFavourite);
-
 Router.get("/favourites/delete/:id", deleteFromFavourite);
+Router.get("/comments", commentsShower);
+// Router.get("/update", updateAccountForm);
+// Router.patch("/update", updateAccount);
+
+// Router.get("/comments/:id/update", commnetsUpdateForm);
+// Router.post("/comments/:id/update", commentsUpdate);
+Router.post("/logout", logOut);
+Router.delete("/comments/:id/delete", commentsDelete);
+Router.delete("/remove", removeAccount);
 
 module.exports = Router;
